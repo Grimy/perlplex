1bc8bb73f969b99b0903083b4d989530cd65f1fb
root <root@deb7xfce>
2014-04-09 19:06:12 +0200

    Use numpy to solve linear equations

--- simplexe.py
+++ simplexe.py
@@ -1,33 +1,34 @@
 import re, sys
 from fractions import Fraction
+from numpy import array
 
-rx = re.compile(r'([-+]?)(\d*)([a-zA-Z]\w+)?')
-lines = [line.replace(' ', '') for line in sys.stdin.readlines()]
-vars = sorted({match.group(3) for match in rx.finditer(''.join(lines))} - {None})
-_ = [dict((match.group(3), match.group(1) + (match.group(2) or '1')) for match in rx.finditer(line)) for line in lines]
-print _
-_ = [[Fraction(coeffs.get(var, 0)) for var in vars + range(1, len(_)) + [None]] for coeffs in _]
-for x in xrange(1, len(_)):
-	_[x][len(vars) - 1 + x] = 1
+def base(p, n):
+	return array([0+(i == p) for i in xrange(n)])
 
-exit(0)
+def index_max(_iter):
+	return max(t[::-1] for t in enumerate(_iter))[1]
+
+lines = sys.stdin.readlines()
+vars = sorted({match.group() for match in re.finditer(r'[a-zA-Z]\w*', ''.join(lines))})
+
+_ = []
+for ln in xrange(len(lines)):
+	line = re.sub(r'(?<=\d)\s*(?=[a-zA-Z\(])', '*', lines[ln].replace('z=', '(').replace('<=', '-(') + ')')
+	coeffs = eval(line, dict((vars[i], base(i, len(vars) + 1)) for i in xrange(len(vars))))
+	_.append(array(map(Fraction, list(coeffs[:-1] - coeffs[-1]) + list(base(ln - 1, len(lines) - 1)) + [-coeffs[-1]])))
 
 cols = vars +   ['e' + str(x) for x in xrange(1, len(_))] + ['b']
 lines = ['z'] + ['e' + str(x) for x in xrange(1, len(_))]
 
-m = 1
-while m > 0:
-	m, y = max(t[::-1] for t in enumerate(l[-1] and l[x] / l[-1] for l in _))
-	m, x = max(t[::-1] for t in enumerate(_[0]))
-	print x, y
+while True:
+	x = index_max(_[0])
+	if _[0][x] <= 0: break
+	y = index_max(l[-1] and l[x] / l[-1] for l in _)
 
-	print '\t'.join(['\n'] + [('[%s]' if i == x else '%s') % cols[i] for i in xrange(len(cols))])
+	print '\t'.join(['\n'] + cols).replace(cols[x], '[%s]' % cols[x])
 	for label, line in zip(lines, _):
-		print '\t'.join([label] + map(str, line))
+		print '\t'.join([label.replace(lines[y], '[%s]' % lines[y])] + map(str, line))
 	
 	(lines[y], cols[x]) = (cols[x], lines[y])
-	_[y] = [t / _[y][x] for t in _[y]]
-
-	for i in set(xrange(len(_))) - {y}:
-		_[i] = [j[0] - _[i][x] * j[1] for j in zip(_[i], _[y])]
+	_ = [l / l[x] if l is _[y] else l - _[y] * l[x] / _[y][x] for l in _]
 
