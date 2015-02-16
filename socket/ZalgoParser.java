0c694ebde99b56726798f0dd928f33a44096cc21
Victor Adam <Victor.Adam@derpymail.org>
2015-02-16 10:35:39 +0100

    ZalgoParser v2

--- socket/ZalgoParser.java
+++ socket/ZalgoParser.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public abstract class ZalgoParser {
 	private static final Pattern ATTR = Pattern.compile("\\s*+=\\s*+(['\"])(.*?)\\1");
-	private static final Pattern TEXT = Pattern.compile("(?:[^<]|<!.*?>)*+");
+	private static final Pattern TEXT = Pattern.compile("(?:[^<]|<!.*?>|<\\?.*?\\?>)*+");
 	private static final Pattern NAME = Pattern.compile("<?\\??\\s*(/?[\\w:\\-]+|/|>)");
 
 	protected abstract void tag(String tagname);
@@ -12,17 +12,20 @@ public abstract class ZalgoParser {
 	
 	public final void parse(Scanner scanner) {
 		String currentTag = null;
-		for (String key = ">";; key = scanner.skip(NAME).match().group(1)) {
-			if (key.equals(">")) {
-				text(scanner.skip(TEXT).match().group());
-				if (!scanner.hasNext()) {
-					return;
+		for (;;) {
+			text(scanner.skip(TEXT).match().group());
+			tag(currentTag = scanner.skip(NAME).match().group(1));
+			if (currentTag.equals("/svg")) {
+				return;
+			}
+			String key = scanner.skip(NAME).match().group(1);
+			while (!key.equals(">")) {
+				if (key.equals("/")) {
+					tag("/" + currentTag);
+				} else {
+					attribute(key, scanner.skip(ATTR).match().group(2));
 				}
-				tag(currentTag = scanner.skip(NAME).match().group(1));
-			} else if (key.equals("/")) {
-				tag("/" + currentTag);
-			} else {
-				attribute(key, scanner.skip(ATTR).match().group(2));
+				key = scanner.skip(NAME).match().group(1);
 			}
 		}
 	}
