ceae5e0f5258c91bb002e4c067504cc3959b536b
Victor Adam <victor.adam@cofelyineo-gdfsuez.com>
2015-09-29 17:52:52 +0200

    QuickFix.

--- unary.pl
+++ unary.pl
@@ -4,58 +4,42 @@ use strict;
 use warnings;
 $" = '|';
 
+sub combinations {
+	my $n = shift() - 1;
+	(@_, $n ? map {my $c = $_; map {"$c$_"} @_} combinations($n, @_) : ());
+}
+
+compare();
+dec2un();
+
 #######
 # Number comparison
 #######
 
-my @shorter = map "\\d{$_}>0*+\\d{$_}\\d+", 0..4;
-my @longer = map "\\d{$_}\\d+>0*+\\d{$_}\$", 0..4;
-my @same = map "\\d{$_}>0*+\\d{$_}\$", 1..5;
-my @lesser = map "$_\\d*>0*+\\1[${\++$_}-9]", 0..8;
-my @greater = map "$_\\d*>0*+\\1[0-${\--$_}]", 1..9;
+sub compare {
+	my @shorter = map "\\d{$_},0*+\\d{$_}\\d+", 0..4;
+	my @longer = map "\\d{$_}\\d+,0*+\\d{$_}\$", 0..4;
+	my @same = map "\\d{$_},0*+\\d{$_}\$", 1..5;
+	my @lesser = map "$_\\d*,0*+\\1[${\++$_}-9]", 0..8;
+	my @greater = map "$_\\d*,0*+\\1[0-${\--$_}]", 1..9;
 
-my $lt = qr/^ 0*+ (?: @shorter | (?=@same) (\d*) (?:@lesser))/x;
-my $gt = qr/^ 0*+ (?: @longer | (?=@same) (\d*) (?:@greater))/x;
+	my $lt = qr/^ 0*+ (?: @shorter | (?=@same) (\d*) (?:@lesser))/x;
+	my $gt = qr/^ 0*+ (?: @longer | (?=@same) (\d*) (?:@greater))/x;
 
-# my $lt = qr/^ 0*+
-	# (?: \d{0} < 0*+ \d{0}\d+
-	  # | \d{1} < 0*+ \d{1}\d+
-	  # | \d{2} < 0*+ \d{2}\d+
-	  # | \d{3} < 0*+ \d{3}\d+
-	  # | \d{4} < 0*+ \d{4}\d+
-	  # | (?= \d{1} < 0*+ \d{1}
-	      # | \d{2} < 0*+ \d{2}
-	      # | \d{3} < 0*+ \d{3}
-	      # | \d{4} < 0*+ \d{4}
-	      # | \d{5} < 0*+ \d{5}
-	      # ) (\d*)
-	    # (?: 0 \d* < 0*+ \1 [1-9]
-	      # | 1 \d* < 0*+ \1 [2-9]
-	      # | 2 \d* < 0*+ \1 [3-9]
-	      # | 3 \d* < 0*+ \1 [4-9]
-	      # | 4 \d* < 0*+ \1 [5-9]
-	      # | 5 \d* < 0*+ \1 [6-9]
-	      # | 6 \d* < 0*+ \1 [7-9]
-	      # | 7 \d* < 0*+ \1 [8-9]
-	      # | 8 \d* < 0*+ \1 [9-9]
-	      # )
-	  # )
-# /x;
+	print "100,100" =~ /$gt/ ? "Yay" : "Nay";
+	print "122,111" =~ /$gt/ ? "Yay" : "Nay";
+	print "2,10"    =~ /$gt/ ? "Yay" : "Nay";
+	print "3,0"    =~ /$gt/ ? "Yay" : "Nay";
 
-print "100>100" =~ /$gt/ ? "Yay" : "Nay";
-print "122>111" =~ /$gt/ ? "Yay" : "Nay";
-print "2>10"    =~ /$gt/ ? "Yay" : "Nay";
-print "3>0"    =~ /$gt/ ? "Yay" : "Nay";
-
-for $a (0..1000) {
-	for $b (0..1000) {
-		for ("$a>$b", "0$a>$b", "$a>0$b", "0$a>0$b") {
-			die $_ if !($a > $b) != !/$gt/;
+	for $a (0..200, 1000, 10000) {
+		for $b (0..200, 1000, 10000) {
+			for ("$a,$b", "0$a,$b", "$a,0$b", "0$a,0$b") {
+				die $_ if ($a > $b) != /$gt/;
+				die $_ if ($a < $b) != /$lt/;
+			}
 		}
 	}
 }
-
-__END__
 
 ########
 # Infinite regex
@@ -65,7 +49,9 @@ __END__
 # Infinite recursion
 # Variable length lookbehind not implemented
 
-# /(.(?2))((?<=(?=(?1)).))/xsm
+sub infinite {
+	/(.(?2))((?<=(?=(?1)).))/xsm;
+}
 
 ########
 # Multiplication
@@ -77,27 +63,26 @@ my $mult = qr/^(1+)x(=|1(?2)\1)$/;
 # Powers
 ########
 
-my $powers = qr/^(1$|(1+)\2(?=\2$)(?1))/;
-print for grep {(1x$_)=~/$powers/} 0..100;
-print "=" x 80;
-
-sub combinations {
-	my $n = shift() - 1;
-	(@_, $n ? map {my $c = $_; map {"$c$_"} @_} combinations($n, @_) : ());
+sub powers {
+	my $powers = qr/^(1$|(1+)\2(?=\2$)(?1))/;
+	print for grep {(1x$_)=~/$powers/} 0..100;
 }
 
-my $r2 = qr/\A [01]*? ([01]) (.*) = (\1?+ (?(3)\3\3)) \z \2 (?>!0) (?!\3)/xsm;
-map {print if /$r2/} combinations 5, 0..2, '=', "\n", 'a';
-print "=" x 80;
+########
+# Bases
+########
 
-# my $rec = qr/\A (= | ([01]) ((?1)) \3 \2?+) \z/xsm;
-# map {print if /$rec/} combinations 5, 0..2, '=', "\n", 'a';
-# print "=" x 80;
+sub bin2un {
+	my $r2 = qr/\A [01]*? ([01]) (.*) = (\1?+ (?(3)\3\3)) \z \2 (?>!0) (?!\3)/xsm;
+	map {print if /$r2/} combinations 5, 0..2, '=', "\n", 'a';
+}
 
-my @digits = 0..9;
-my $base = @digits;
-@digits = map qr{$digits[$_] (.*?) = ( (?(2)\2{$base}) 1{$_} )}x, 0..$#digits;
-my $r10 = qr/\A .*? (?>(?| @digits | (*PRUNE)(*FAIL) )) \z \1/xsm;
+sub dec2un {
+	my @digits = 0..9;
+	my $base = @digits;
+	@digits = map qr{$digits[$_] (.*?) = ( (?(2)\2{$base}) 1{$_} )}x, 0..$#digits;
+	my $r10 = qr/\A .*? (?>(?| @digits | (*PRUNE)(*FAIL) )) \z \1/xsm;
 
-map {print if /$r10/} combinations 4, 0..9, '=', "\n", 'a';
-map {print if /$r10/} map {"19=" . 1x$_} 0..100;
+	map {print if /$r10/} combinations 4, 0..9, '=', "\n", 'a';
+	map {print if /$r10/} map {"19=" . 1x$_} 0..100;
+}
